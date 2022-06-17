9544483a2b4a24d2fcc8415cd246d98dc3bcbce9
unknown <divyasriambati123@gmail.com>
2022-06-17 10:37:48 +0530

    intialize all componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ClientsComponent } from './clients/clients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent,
+    HomeComponent,
+    ClientsComponent
   ],
   imports: [
     BrowserModule,
