9c4507f7c45f56d937dafb8293a18b77a1485e0d
Praveen <dpraveen453@gmail.com>
2022-06-20 11:47:26 +0530

    committed changes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '/orangestack')))
+app.use(express.static(path.join(__dirname, '/dist/orangestack')))
 
 // app.get('/',function(req,res){
 //     res.send("it's working")
